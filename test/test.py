f80845e13cc3d627fe6ea75d10217260008f9bd0
spark1217 <102626805+spark1217@users.noreply.github.com>
2022-10-03 12:12:56 -0400

    Create test.py

---
+++ test/test.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append('src')
+import config
+import discord
+from discord.ext import commands
+import finnhub
+
+# tokens
+DISCORD_TOKEN = config.tokens['discord_token']
+FINNHUB_TOKEN = config.tokens['finnhub_token']
+
+client = commands.Bot(command_prefix='!', intents=discord.Intents.all()) #discord.Client(intents=discord.Intents.all())
+
+# setup finnhub
+finnhub_client = finnhub.Client(api_key=FINNHUB_TOKEN)
+
+# print message when ready
+@client.event
+async def on_ready_test():
+    print('Logged in {0.user}'.format(client))
